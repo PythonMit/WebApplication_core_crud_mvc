3cf4ed23ede182fe5be6efbb74acc084b3809001
mitesh.n.gandhi <mitesh.n.gandhi@TIPL23>
2025-03-31 15:11:45 +0530

    add confirmation for delete product and set total price

--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -6,5 +6,6 @@
         public string? Name { get; set; }
         public int Price { get; set; }
         public int Stock { get; set; }
+        public int TotalPrice { get { return Price * Stock; } }
     }
 }
